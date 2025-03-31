7dc50cafdd456331039793fba5c6dcf5a53caa0c
raphaeldesosa <des22005@byui.edu>
2025-04-01 01:47:09 +0800

    added breadcrumbs to product list page

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -13,6 +13,18 @@ const myList = new ProductList(category, dataSource, listElement);
 // finally call the init method to show the products
 myList.init();
 
+if (category) {
+  setTimeout(() => {
+    const breadcrumbs = document.getElementById("breadcrumbs");
+    const itemCount = listElement.children.length;
+    breadcrumbs.textContent = `${category} -> (${itemCount} items)`;
+
+    breadcrumbs.style.padding = "10px";
+    breadcrumbs.style.backgroundColor = "#f8f8f8";
+    breadcrumbs.style.fontWeight = "bold";
+  }, 500);
+}
+
 document.getElementById("modalClose").onclick = () => {
   document.getElementById("modal").classList.add("hide");
 }
